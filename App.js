ea11e5692197298f696d01893aa8f76eef2551e4
white piano <startoflateness@gmail.com>
2023-06-19 15:54:50 +0900

    view todo list 기능 구현

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  ActivityIndicator,
+  View,
+  ToastAndroid,
+} from 'react-native';
+import { useEffect, useState } from 'react';
+import Constants from 'expo-constants';
+import TodoList from './components/TodoList';
 
 export default function App() {
+  const [loadingCount, setLoadingCount] = useState(0);
+  const [todoList, setTodoList] = useState([]);
+
+  const fetchTodoList = () => {
+    setLoadingCount((prev) => prev + 1);
+
+    fetch(Constants.expoConfig.extra.apiServer + '/todos', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(res.status);
+
+        return res.json();
+      })
+      .then((body) => {
+        const { message, data } = body;
+        if (!data) throw new Error(message);
+
+        setTodoList(data);
+      })
+      .catch((err) => {
+        ToastAndroid.show(
+          'fail to fetch todo list, please re-start application',
+          ToastAndroid.LONG
+        );
+      })
+      .finally(() => {
+        setLoadingCount((prev) => prev - 1);
+      });
+  };
+
+  useEffect(fetchTodoList, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={styles.container}
+      pointerEvents={loadingCount === 0 ? 'auto' : 'none'}
+    >
+      <TodoList todoList={todoList} />
       <StatusBar style="auto" />
+      {loadingCount !== 0 && (
+        <View style={styles.activityIndicatorContainer}>
+          <ActivityIndicator />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +65,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  activityIndicatorContainer: {
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
+    position: 'absolute',
+    backgroundColor: '#ecf0f180',
   },
 });
