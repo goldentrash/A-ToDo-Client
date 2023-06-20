fca8f759a28ec95633d2b68c93e377dfe152d331
white piano <startoflateness@gmail.com>
2023-06-20 16:09:13 +0900

    start doing 기능 구현, todo list style 조정

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,28 +1,116 @@
-import { FlatList, Text, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  FlatList,
+  Modal,
+  Pressable,
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
 
-const TodoList = ({ todoList }) => {
+const TodoList = ({ todoList, callApiThenFetchTodoList }) => {
+  const [startDoingModalVisible, setStartDoingModalVisible] = useState(false);
+  const openStartDoingModal = () => setStartDoingModalVisible(true);
+  const closeStartDoingModal = () => setStartDoingModalVisible(false);
+
+  const [watchingTodo, setWatchingTodo] = useState(null);
   const renderTodo = ({ item: todo }) => {
     const { content, deadline } = todo;
+    const watchTodo = () => {
+      setWatchingTodo(todo);
+      openStartDoingModal();
+    };
 
     return (
-      <View style={styles.listItem}>
-        <Text>{content}</Text>
-        <Text style={styles.deadlineText}>{deadline.toString()}</Text>
-      </View>
+      <Pressable style={styles.listItem} onPress={watchTodo}>
+        <Text style={styles.listItemTextContent}>{content}</Text>
+        <Text style={styles.listItemTextDeadline}>{deadline.toString()}</Text>
+      </Pressable>
     );
   };
 
   return (
-    <FlatList
-      style={styles.container}
-      data={todoList}
-      renderItem={renderTodo}
-    />
+    <>
+      <FlatList
+        style={styles.listContainer}
+        data={todoList}
+        renderItem={renderTodo}
+      />
+
+      {startDoingModalVisible && (
+        <StartDoingModal
+          callApiThenFetchTodoList={callApiThenFetchTodoList}
+          onRequestClose={closeStartDoingModal}
+          watchingTodo={watchingTodo}
+        />
+      )}
+    </>
+  );
+};
+
+const StartDoingModal = ({
+  callApiThenFetchTodoList,
+  onRequestClose,
+  watchingTodo: { id, content, deadline },
+}) => {
+  const startDoingThenFetchTodoList = callApiThenFetchTodoList(
+    { path: '/doings', method: 'POST', body: { id } },
+    onRequestClose
+  );
+
+  return (
+    <Modal onRequestClose={onRequestClose}>
+      <View style={styles.modalBackground}>
+        <View style={styles.modalForeground}>
+          <Text style={styles.modalTitle}>Start Doing</Text>
+
+          <View style={styles.modalTextContainer}>
+            <Text style={styles.modalTextContent}>{content}</Text>
+            <Text style={styles.modalTextDeadline}>{deadline}</Text>
+          </View>
+
+          <Button onPress={startDoingThenFetchTodoList} title="start doing" />
+        </View>
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f180',
+  },
+  modalForeground: {
+    backgroundColor: '#ffffff',
+    width: '80%',
+    height: 380,
+    gap: 12,
+    padding: 32,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  modalTextContainer: {
+    gap: 8,
+    borderTopWidth: 1,
+    height: 220,
+    paddingVertical: 16,
+  },
+  modalTextContent: {
+    fontSize: 16,
+  },
+  modalTextDeadline: {
+    fontSize: 12,
+    textAlign: 'right',
+  },
+  listContainer: {
     flex: 1,
     marginTop: 64,
     width: '80%',
@@ -32,9 +120,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 6,
     margin: 8,
-    padding: 4,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
   },
-  deadlineText: {
+  listItemTextContent: {
+    fontSize: 16,
+  },
+  listItemTextDeadline: {
     fontSize: 12,
     textAlign: 'right',
   },
