9a41950947cfc4f3777e1a8bd16c644934bb8491
white piano <startoflateness@gmail.com>
2023-07-10 17:08:00 +0900

    husky, lint-staged, eslint, prettier

--- components/TodoListPage.tsx
+++ components/TodoListPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   Button,
   FlatList,
@@ -8,9 +8,9 @@ import {
   StyleSheet,
   ScrollView,
   View,
-} from 'react-native';
-import type { GenCallApi } from 'types/api';
-import type { Todo } from 'types/todo';
+} from "react-native";
+import type { GenCallApi } from "types/api";
+import type { Todo } from "types/todo";
 
 type TodoListPageProps = {
   todoList: Todo[];
@@ -56,6 +56,7 @@ export default function TodoListPage({
             genCallApiThenFetchTodoListAndDoing
           }
           onRequestClose={closeStartTodoModal}
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
           watchingTodo={watchingTodo!}
         />
       )}
@@ -74,7 +75,7 @@ function StartTodoModal({
   watchingTodo: { id, content, deadline },
 }: StartTodoModalProps) {
   const startTodoThenFetchTodoList = genCallApiThenFetchTodoListAndDoing(
-    { path: '/doings', method: 'POST', body: { id } },
+    { path: "/doings", method: "POST", body: { id } },
     onRequestClose
   );
 
@@ -101,28 +102,45 @@ function StartTodoModal({
 }
 
 const styles = StyleSheet.create({
-  modalBackground: {
+  listContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ecf0f180',
+    marginTop: 64,
+    maxHeight: "75%",
+    width: "80%",
+  },
+  listItem: {
+    borderColor: "black",
+    borderRadius: 6,
+    borderWidth: 1,
+    margin: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+  },
+  listItemTextContent: {
+    fontSize: 16,
+  },
+  listItemTextDeadline: {
+    fontSize: 12,
+    textAlign: "right",
+  },
+  modalBackground: {
+    alignItems: "center",
+    backgroundColor: "#ecf0f180",
+    flex: 1,
+    justifyContent: "center",
   },
   modalForeground: {
-    backgroundColor: '#ffffff',
-    width: '80%',
-    height: 380,
-    gap: 12,
-    padding: 32,
+    backgroundColor: "#ffffff",
     borderRadius: 8,
     borderWidth: 1,
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    gap: 12,
+    height: 380,
+    padding: 32,
+    width: "80%",
   },
   modalTextContainer: {
-    gap: 8,
     borderTopWidth: 1,
+    gap: 8,
     height: 220,
     paddingVertical: 16,
   },
@@ -131,27 +149,10 @@ const styles = StyleSheet.create({
   },
   modalTextDeadline: {
     fontSize: 12,
-    textAlign: 'right',
+    textAlign: "right",
   },
-  listContainer: {
-    flex: 1,
-    marginTop: 64,
-    width: '80%',
-    maxHeight: '75%',
-  },
-  listItem: {
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 6,
-    margin: 8,
-    paddingVertical: 6,
-    paddingHorizontal: 12,
-  },
-  listItemTextContent: {
-    fontSize: 16,
-  },
-  listItemTextDeadline: {
-    fontSize: 12,
-    textAlign: 'right',
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
